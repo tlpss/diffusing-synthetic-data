88e64989eb06b05f1a1fb2e36337628462c1f998
tlips <thomas17.lips@gmail.com>
2024-01-24 09:32:45 +0100

    init from template

---
+++ dsd/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from setuptools import find_packages
+
+setuptools.setup(
+    name="dsd",
+    version="0.0.1",
+    author="Thomas Lips",
+    author_email="thomas.lips@ugent.be",
+    description="TODO",
+    install_requires=[
+        "numpy",
+    ],
+    packages=find_packages(),
+)
