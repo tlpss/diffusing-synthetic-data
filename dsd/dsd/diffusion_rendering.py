56adfb1f7b154da6b6b103b4d5d32307896ecf75
tlpss <thomas17.lips@gmail.com>
2024-02-05 14:18:52 +0100

    add SDXL and SD2.0

--- dsd/dsd/diffusion_rendering.py
+++ dsd/dsd/diffusion_rendering.py
@@ -12,7 +12,15 @@ import cv2
 import numpy as np
 import torch
 from controlnet_aux import CannyDetector, PidiNetDetector
-from diffusers import ControlNetModel, StableDiffusionControlNetImg2ImgPipeline
+from diffusers import (
+    AutoencoderKL,
+    ControlNetModel,
+    DiffusionPipeline,
+    StableDiffusionControlNetImg2ImgPipeline,
+    StableDiffusionDepth2ImgPipeline,
+    StableDiffusionInpaintPipeline,
+    StableDiffusionXLControlNetPipeline,
+)
 from PIL import Image
 
 
@@ -22,6 +30,8 @@ class DiffusionRenderInputImages:
     """ (H,W,3) RGB image as numpy array in range [0,1]"""
     depth_image: np.ndarray
     """ (H,W) absolute depth map as numpy array"""
+    mask: np.ndarray
+    """ (H,W) mask as numpy array, 1.0 for object pixels, 0.0 for background pixels"""
 
     @classmethod
     def from_render_dir(cls, render_dir: str):
@@ -36,14 +46,20 @@ class DiffusionRenderInputImages:
         # controlnet actually uses relative, inverted depth
         # cf. Midas model https://pytorch.org/hub/intelisl_midas_v2/
         # depth_image = invert_and_normalize_depth_image(depth_image)
-        return cls(rgb_image, depth_image)
+
+        mask = Image.open(render_dir / "segmentation.png")
+        mask = np.array(mask)
+        mask = mask.astype(np.float32) / 255.0
+        return cls(rgb_image, depth_image, mask)
 
     def get_rgb_image_torch(self):
+        rgb_image_torch = np.copy(self.rgb_image)
         rgb_image_torch = torch.from_numpy(self.rgb_image).permute(2, 0, 1).unsqueeze(0)
         return rgb_image_torch
 
     def get_inverted_depth_image_torch(self):
-        inverted_depth_image = _invert_and_normalize_depth_image(self.depth_image)
+        depth_image = np.copy(self.depth_image)
+        inverted_depth_image = _invert_and_normalize_depth_image(depth_image)
         inverted_depth_image = torch.from_numpy(inverted_depth_image)
         # duplicate 3 channels
         inverted_depth_image = inverted_depth_image.repeat(3, 1, 1).unsqueeze(0)
@@ -61,6 +77,10 @@ class DiffusionRenderInputImages:
         normal_map = self.get_normal_map()
         normal_map = torch.from_numpy(normal_map).permute(2, 0, 1).unsqueeze(0)
         return normal_map
+
+    def get_mask_torch(self):
+        mask_torch = torch.from_numpy(self.mask).unsqueeze(0).unsqueeze(0)
+        return mask_torch
 
 
 def _get_controlnet_like_normals_from_depth_image(depth_image: np.ndarray, sobel_kernel_size: int = 7) -> np.ndarray:
@@ -109,12 +129,76 @@ class DiffusionRenderer:
         return self.__class__.__name__
 
 
-class SDInpaintingRenderer(DiffusionRenderer):
-    pass
+class SD2InpaintingRenderer(DiffusionRenderer):
+    """
+    Stable Diffusion 2 Inpainting model
+    https://huggingface.co/stabilityai/stable-diffusion-2-inpainting
+
+    """
+
+    def __init__(self, num_images_per_prompt=4, num_inference_steps=50, strength=1.0):
+        super().__init__(num_images_per_prompt, num_inference_steps)
+        self.strength = strength
+        self.pipe = StableDiffusionInpaintPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-inpainting",
+            torch_dtype=torch.float16,
+        ).to("cuda")
+
+    def __call__(self, prompt, input_images, **kwargs):
+        rgb_image = input_images.get_rgb_image_torch()
+        mask = input_images.get_mask_torch()
+
+        output_dict = self.pipe(
+            prompt=prompt,
+            image=rgb_image,
+            mask_image=mask,
+            num_images_per_prompt=self.num_images_per_prompt,
+            num_inference_steps=self.num_inference_steps,
+            strength=self.strength,
+            generator=self.generator,
+        )
+        images = output_dict.images
+        return images
 
 
-class SDFromDepthRenderer(DiffusionRenderer):
-    pass
+class SD2FromDepthRenderer(DiffusionRenderer):
+    """
+    Stable Diffusion 2 + native depth conditioning
+    https://huggingface.co/stabilityai/stable-diffusion-2-depth
+
+
+    use the GenAug settings for this model.
+    """
+
+    def __init__(self, num_images_per_prompt=4, num_inference_steps=50, strength=0.9):
+        super().__init__(num_images_per_prompt, num_inference_steps)
+        self.strength = strength
+
+        self.pipe = StableDiffusionDepth2ImgPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-2-depth",
+            torch_dtype=torch.float16,
+        ).to("cuda")
+
+    def __call__(self, prompt, input_images, **kwargs):
+        # depth_image = input_images.get_inverted_depth_image_torch()
+        # use settings as in GenAug: no GT depth
+
+        # rgb_image = input_images.get_rgb_image_torch()
+
+        # TOOD: figure out why directly passing the torch tensor does not work?
+        rgb_image = Image.fromarray((input_images.rgb_image * 255).astype(np.uint8))
+
+        output_dict = self.pipe(
+            prompt=prompt,
+            image=rgb_image,
+            negative_prompt="bad, deformed, ugly, bad anotomy, low resolution",
+            num_images_per_prompt=self.num_images_per_prompt,
+            num_inference_steps=self.num_inference_steps,
+            strength=self.strength,
+            generator=self.generator,
+        )
+        images = output_dict.images
+        return images
 
 
 class ControlNetRenderer(DiffusionRenderer):
@@ -131,18 +215,18 @@ class ControlNetRenderer(DiffusionRenderer):
         super().__init__(num_images_per_prompt, num_inference_steps)
         self.controlnet_conditioning_scale = controlnet_conditioning_scale
         self.strength = strength
-        self.pipe: StableDiffusionControlNetImg2ImgPipeline = None
+        self.pipe: DiffusionPipeline = None
 
     def get_logging_name(self):
-        return f"{self.__class__.__name__}_cgs={self.controlnet_conditioning_scale}"
+        return f"{self.__class__.__name__}_ccs={self.controlnet_conditioning_scale}"
 
     def __call__(self, prompt, input_images, **kwargs):
-        # TODO: better to resize in the pipeline?
-        assert input_images.rgb_image.shape[:2] == self.input_resolution
+        img = self.preprocess_rgb_image(input_images)
+        control = self.get_control_image(input_images)
         output_dict = self.pipe(
             prompt=prompt,
-            image=input_images.get_rgb_image_torch(),
-            control_image=self.get_control_image(input_images),
+            image=img,
+            control_image=control,
             num_images_per_prompt=self.num_images_per_prompt,
             controlnet_conditioning_scale=self.controlnet_conditioning_scale,
             strength=self.strength,
@@ -155,15 +239,104 @@ class ControlNetRenderer(DiffusionRenderer):
     def get_control_image(self, input_images: DiffusionRenderInputImages):
         raise NotImplementedError
 
+    def preprocess_rgb_image(self, input_images: DiffusionRenderInputImages) -> torch.Tensor:
+        rgb_image = input_images.get_rgb_image_torch()
+        if rgb_image.shape[:2] != self.input_resolution:
+            rgb_image = torch.nn.functional.interpolate(rgb_image, size=self.input_resolution, mode="bicubic")
+        return rgb_image
+
 
 class SDXLControlNetFromDepthRenderer(ControlNetRenderer):
     """
-
     SD-XL + Controlnet, depth conditioned.
-
-
-
+    https://huggingface.co/docs/diffusers/api/pipelines/controlnet_sdxl#diffusers.StableDiffusionXLControlNetImg2ImgPipeline
     """
+
+    def __init__(
+        self,
+        num_images_per_prompt: int = 4,
+        num_inference_steps: int = 50,
+        controlnet_conditioning_scale: float = ControlNetRenderer.DEFAULT_CONTROLNET_CONDITIONING_SCALE,
+        strength: float = ControlNetRenderer.DEFAULT_STRENGTH,
+    ):
+        super().__init__(num_images_per_prompt, num_inference_steps, controlnet_conditioning_scale, strength)
+        self.input_resolution = (1024, 1024)  # SD-XL is trained on 1024x1024 images
+
+        self.controlnet = ControlNetModel.from_pretrained(
+            "diffusers/controlnet-depth-sdxl-1.0-small",
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to("cuda")
+        self.vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
+        self.pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            controlnet=self.controlnet,
+            vae=self.vae,
+            variant="fp16",
+            torch_dtype=torch.float16,
+            use_safetensors=True,
+        ).to("cuda")
+        self.pipe.enable_model_cpu_offload()
+
+    def get_control_image(self, input_images: DiffusionRenderInputImages):
+        depth_image = input_images.get_inverted_depth_image_torch()
+        # resize the images to 1024x1024
+        depth_image = torch.nn.functional.interpolate(depth_image, size=(1024, 1024), mode="bicubic")
+        # return torch.randn(1, 3, 1024, 1024)
+        return depth_image
+
+    def __call__(self, prompt, input_images, **kwargs):
+
+        images = super().__call__(prompt, input_images, **kwargs)
+        images = [image.resize((512, 512)) for image in images]
+        return images
+
+
+class SDXLControlNetFromCannyRenderer(ControlNetRenderer):
+    """
+    SD-XL + Controlnet, canny conditioned.
+    https://huggingface.co/docs/diffusers/api/pipelines/controlnet_sdxl#diffusers.StableDiffusionXLControlNetImg2ImgPipeline
+    """
+
+    def __init__(
+        self,
+        num_images_per_prompt: int = 4,
+        num_inference_steps: int = 50,
+        controlnet_conditioning_scale: float = ControlNetRenderer.DEFAULT_CONTROLNET_CONDITIONING_SCALE,
+        strength: float = ControlNetRenderer.DEFAULT_STRENGTH,
+    ):
+        super().__init__(num_images_per_prompt, num_inference_steps, controlnet_conditioning_scale, strength)
+        self.input_resolution = (1024, 1024)  # SD-XL is trained on 1024x1024 images
+
+        self.controlnet = ControlNetModel.from_pretrained(
+            "diffusers/controlnet-canny-sdxl-1.0",
+            variant="fp16",
+            use_safetensors=True,
+            torch_dtype=torch.float16,
+        ).to("cuda")
+        self.vae = AutoencoderKL.from_pretrained("madebyollin/sdxl-vae-fp16-fix", torch_dtype=torch.float16).to("cuda")
+        self.pipe = StableDiffusionXLControlNetPipeline.from_pretrained(
+            "stabilityai/stable-diffusion-xl-base-1.0",
+            controlnet=self.controlnet,
+            vae=self.vae,
+            variant="fp16",
+            torch_dtype=torch.float16,
+            use_safetensors=True,
+        ).to("cuda")
+        self.pipe.enable_model_cpu_offload()
+
+    def get_control_image(self, input_images: DiffusionRenderInputImages):
+        depth_image = input_images.get_inverted_depth_image_torch()
+        # resize the images to 1024x1024
+        depth_image = torch.nn.functional.interpolate(depth_image, size=(1024, 1024), mode="bicubic")
+        return depth_image
+
+    def __call__(self, prompt, input_images, **kwargs):
+
+        images = super().__call__(prompt, input_images, **kwargs)
+        images = [image.resize((512, 512)) for image in images]
+        return images
 
 
 class ControlNetFromDepthRenderer(ControlNetRenderer):
