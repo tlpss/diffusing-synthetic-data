f50ee96e1b22902481a8f9b459a1656e8025f092
tlpss <thomas17.lips@gmail.com>
2024-05-23 10:24:47 +0200

    generate random textures

---
+++ dsd/experiments/paths.py
@@ -0,0 +1,17 @@
+from dsd import DATA_DIR
+
+# scenes
+SHOE_SCENES_DIR = DATA_DIR / "scenes" / "shoes" / "gso-filtered-2500"
+MUG_SCENES_DIR = DATA_DIR / "scenes" / "mugs" / "objaverse-filtered-2500-ral"
+TSHIRT_SCENES_DIR = DATA_DIR / "scenes" / "tshirts" / "syncloth-filtered-2500"
+
+# real datasets
+
+# random texture baselines
+RANDOM_TEXTURE_BASELINE_DIR = DATA_DIR / "random_textures"
+SHOE_RANDOM_TEXTURE_RENDER_DIR = RANDOM_TEXTURE_BASELINE_DIR / "shoes" / "001"
+MUG_RANDOM_TEXTURE_RENDER_DIR = RANDOM_TEXTURE_BASELINE_DIR / "mugs" / "001"
+TSHIRT_RANDOM_TEXTURE_RENDER_DIR = RANDOM_TEXTURE_BASELINE_DIR / "tshirts" / "001"
+
+
+# diffusion render dirs
