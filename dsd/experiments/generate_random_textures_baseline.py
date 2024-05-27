eb4b4850c0a56dc301c415680092f8eab79639eb
tlpss <thomas17.lips@gmail.com>
2024-05-27 13:48:06 +0200

    random textures baselines

--- dsd/experiments/generate_random_textures_baseline.py
+++ dsd/experiments/generate_random_textures_baseline.py
@@ -6,7 +6,14 @@ from dsd.render_random_textures import generate_random_texture_renders
 
 sys.path.append(str(DATA_DIR.parent / "dsd" / "experiments"))
 
-from paths import SHOE_RANDOM_TEXTURE_RENDER_DIR, SHOE_SCENES_DIR
+from paths import (
+    MUG_RANDOM_TEXTURE_RENDER_DIR,
+    MUG_SCENES_DIR,
+    SHOE_RANDOM_TEXTURE_RENDER_DIR,
+    SHOE_SCENES_DIR,
+    TSHIRT_RANDOM_TEXTURE_RENDER_DIR,
+    TSHIRT_SCENES_DIR,
+)
 
 num_renders_per_scene = 2
 
@@ -16,14 +23,14 @@ generate_random_texture_renders(
     num_renders_per_scene=num_renders_per_scene,
 )
 
-# generate_random_texture_renders(
-#     source_directory=MUG_SCENES_DIR,
-#     target_directory=MUG_RANDOM_TEXTURE_RENDER_DIR,
-#     num_renders_per_scene=num_renders_per_scene,
-# )
+generate_random_texture_renders(
+    source_directory=MUG_SCENES_DIR,
+    target_directory=MUG_RANDOM_TEXTURE_RENDER_DIR,
+    num_renders_per_scene=num_renders_per_scene,
+)
 
-# generate_random_texture_renders(
-#     source_directory=TSHIRT_SCENES_DIR,
-#     target_directory=TSHIRT_RANDOM_TEXTURE_RENDER_DIR,
-#     num_renders_per_scene=num_renders_per_scene,
-# )
+generate_random_texture_renders(
+    source_directory=TSHIRT_SCENES_DIR,
+    target_directory=TSHIRT_RANDOM_TEXTURE_RENDER_DIR,
+    num_renders_per_scene=num_renders_per_scene,
+)
