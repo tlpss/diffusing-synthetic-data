00cc07fc423e0fbd338b922ef835bafad618fa5e
tlpss <thomas17.lips@gmail.com>
2024-05-29 10:01:42 +0200

    docker updates

--- docker/build_and_push_docker.sh
+++ docker/build_and_push_docker.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 
-cd .. && docker build -f docker/Dockerfile . -t dsd:cvpr-syntagen
-docker tag dsd:cvpr-syntagen tlpss/dsd:cvpr-syntagen
-docker push tlpss/dsd:cvpr-syntagen
+cd .. && docker build -f docker/Dockerfile . -t dsd:ral
+docker tag dsd:ral tlpss/dsd:ral-v1
+docker push tlpss/dsd:ral-v1
